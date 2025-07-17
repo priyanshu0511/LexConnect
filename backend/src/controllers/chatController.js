a0ba5f5dc5f34b865a7882efeae8e915232d4ff9
priyanshu0511 <priyanshu05112@gmail.com>
2025-07-18 01:07:16 +0530

    fixed return format in getStreamToken

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -1,11 +1,11 @@
-import { generateStreamToken } from "../lib/stream.js"
+import { generateStreamToken } from "../lib/stream.js";
 
-export const getStreamToken = async (req,res) => {
-    try {
-        const token = generateStreamToken(req.user.id);
-        res.status(200).json(token);
-    } catch (error) {
-        console.log("Error in getStreamToken Controller :",error);
-        res.status(500).json({message:"Internal Server Error"});
-    }
-}
+export const getStreamToken = async (req, res) => {
+  try {
+    const token = generateStreamToken(req.user.id);
+    res.status(200).json({ token });
+  } catch (error) {
+    console.log("Error in getStreamToken Controller :", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
