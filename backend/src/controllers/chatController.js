eae581ea62645e2910052ce60078767e92be031a
priyanshu0511 <priyanshu05112@gmail.com>
2025-07-18 01:14:43 +0530

    fixed getStreamToken

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -2,7 +2,7 @@ import { generateStreamToken } from "../lib/stream.js";
 
 export const getStreamToken = async (req, res) => {
   try {
-    const token = generateStreamToken(req.user.id);
+    const token = await generateStreamToken(req.user.id);
     res.status(200).json({ token });
   } catch (error) {
     console.log("Error in getStreamToken Controller :", error);
