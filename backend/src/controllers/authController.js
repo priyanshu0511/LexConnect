337507d8abb9f816ec47cb21d92363447a83c09c
priyanshu0511 <priyanshu05112@gmail.com>
2025-07-22 21:33:12 +0530

    fixed secure cookies

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -57,8 +57,7 @@ export const signup = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
       sameSite: "None",
-      secure: process.env.NODE_ENV === "production",
-
+      secure: true,
     });
 
     res.status(201).json({ success: true, user: newUser });
@@ -95,7 +94,7 @@ export const login = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
       sameSite: "None",
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
     });
 
     res.status(200).json({ success: true, user });
